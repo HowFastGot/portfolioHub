7b9cf5276951cfcd45076336db8b63cd621a22b4
Yevhen <skaretskiy1999@gmail.com>
2024-10-19 16:47:08 +0300

    A typo of parentheses was solved

--- src/js/modules/samples/showMoreStyles.js
+++ src/js/modules/samples/showMoreStyles.js
@@ -1,4 +1,4 @@
-import {getResource} from "../services/requests.js";
+import { getResource } from '../services/requests.js';
 
 const showMoreStyles = (triggerSelector, wrapperSelector) => {
 	let btn = document.querySelector(triggerSelector);
@@ -15,22 +15,22 @@ const showMoreStyles = (triggerSelector, wrapperSelector) => {
 	// 	this.remove();
 	// });
 
-	btn.addEventListener("click", function(e) {
-		getResource("http://localhost:3000/styles")
-			.then(res => createCards(res))
-			.catch(err => console.log("Ошибка получения данных" + err.message));
+	btn.addEventListener('click', function (e) {
+		getResource('http://localhost:3000/styles')
+			.then((res) => createCards(res))
+			.catch((err) => console.log('Ошибка получения данных' + err.message));
 
-			this.remove();
+		this.remove();
 	});
 
 	function createCards(response) {
-		response.forEach(({src, title, link}) => {
-			let div = document.createElement("div");
-			div.classList.add("animated", "fadeInUp", "col-sm-3", "col-sm-offset-0", "col-xs-10", "col-xs-offset-1");
+		response.forEach(({ src, title, link }) => {
+			let div = document.createElement('div');
+			div.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
 
 			div.innerHTML = `
 				<div class="styles-block">
-					<img src=${src} alt="style">
+					<img src="${src} alt="style">
 					<h4>${title}</h4>
 					<a href="${link}">Подробнее</a>
 				</div>
